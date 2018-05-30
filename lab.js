3fb13097671171f3cc21c358453c43f595202e1a
Dav1001 <studenta@aitc.eif.local>
2018-05-30 21:16:29 +0400

    first

---
+++ lab.js
@@ -0,0 +1,8 @@
+let user = {
+	name:'Davit',
+	age: 19,
+	toString: function(){
+		return this.name;
+	}
+}
+console.log(user.toString());
---
+++ lab1.js
@@ -0,0 +1,42 @@
+ function randomValue(max) {
+           return Math.round(Math.random()*max);
+       }
+
+       // let a, b, c;
+
+       // function getA(callback){
+       //     setTimeout( function(){
+       //         a = randomValue(5);
+       //         console.log(a);
+       //         callback(a);
+               
+       //     }, 1000 );
+
+       // }
+
+       // function getB (value, callout){
+       //     setTimeout(function(){
+       //         b = randomValue(value);
+       //         console.log(b);
+       //         callout(b);
+       //     }, 1000);
+
+       // }
+
+       // function getC (key){
+       //  setTimeout(function(){
+       //        c = randomValue(key);
+       //        console.log(c);
+       //        end();
+       //  }, 1000)
+       // }
+
+       // function end(){
+       //     console.log('a = ' + a);
+       //     console.log('b = ' + b);
+       //     console.log('c = ' + c);
+       // }
+
+            
+        
+       // getA(getB,getC);
